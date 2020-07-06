9a31c01c04e1b4a26aff4c57587003d24f749a88
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-07-07 01:37:48 +0530

    BMI Calculator

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,18 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Geetansh Atrey on 07/07/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
